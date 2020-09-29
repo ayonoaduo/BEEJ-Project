c6d148b24902729eae50a1f5ace7528800a32987
Onisokien Ayonoadu <55804238+ayonoaduo@users.noreply.github.com>
2020-09-28 19:16:45 -0600

    Delete GameSubject.java

--- Code/GameSubject.java
+++
@@ -1,8 +0,0 @@
-package com.trevordouglas;
-
-public interface GameSubject 
-{
-	public void addObserver(GameObserver gameObserver);
-	public void removeObserver(GameObserver gameObserver);
-	public void doNotify();
-}
